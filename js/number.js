dfb78622d7755a51a521dbe485c7b4539f5cf3b8
binwoonki <vinwoonki98@gmail.com>
2024-06-25 18:03:15 +0900

    기타 수학 함수 사용해보기(random, max, min, pow)

--- js/number.js
+++ js/number.js
@@ -16,10 +16,25 @@ console.log(Math.ceil(num2)); // 10
 // 소수점 첫째 자리에서 반올림
 let num3 = 4.31;
 
-console.log(Math.round(num3));
+console.log(Math.round(num3)); // 4
 
 // Math.trunc()
 // 소수부를 무시.
 let num4 = 2.3;
 
-console.log(Math.trunc(num4));
+console.log(Math.trunc(num4)); // 2
+
+// 기타 수학 함수
+
+// Math.random()
+// 0과 1 사이의 난수를 반환한다. 1은 제외
+console.log(Math.random());
+
+// Math.max(), Math.min()
+// 인수중 최대/최솟값을 반환한다.
+console.log(Math.max(3, 5, 1, 2, -20, 9)); // 9
+console.log(Math.min(1, 4, -19)); // -19
+
+// Math.pow(n, power)
+// n을 power번 거듭제곱한 값을 반환한다.
+console.log(Math.pow(2, 3)); // 8
