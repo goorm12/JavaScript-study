1a46ad0c9e6c34a6c1a4fe818c1c00e2fb8ee17e
binwoonki <vinwoonki98@gmail.com>
2024-06-25 19:07:39 +0900

    random-number js 완료

--- js/random-number.js
+++ js/random-number.js
@@ -1,1 +1,10 @@
+const button = document.querySelector(".buttonBox__button");
+const number = document.querySelector(".buttonBox__sub-title");
 
+function randomButton(e) {
+  const randomNumber = Math.floor(Math.random() * 10 + 1);
+  e.preventDefault();
+  number.innerHTML = randomNumber;
+}
+
+button.addEventListener("click", randomButton);
