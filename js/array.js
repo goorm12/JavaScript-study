a1aa3ae2ccc41022d0e0c782cb1b3a34b4f8df65
binwoonki <vinwoonki98@gmail.com>
2024-06-26 18:45:55 +0900

    array.splice(), array.slice() 사용해보기

--- js/array.js
+++ js/array.js
@@ -35,3 +35,26 @@ console.log(season.shift()); // 'spring'
 season.unshift("spring");
 
 console.log(season); // ["spring", "summer", "autumn", "winter"]
+
+// splice: 만능 스위스 맥가이버 칼 같은 메서드,,,,,추가,삭제,교체 모두 가능!
+
+let apple = ["ipad", "iphone", "mac"];
+
+// 추가
+apple.splice(3, 0, "airpods"); // ["ipad", "iphone", "mac", "airpods"]
+console.log(apple);
+
+// 삭제
+apple.splice(1, 1); // 인덱스 1번이 가리키는 요소부터 시작해 요소 1개 지우기
+console.log(apple);
+
+// 음수도 가능
+apple.splice(-1, 0, "vision");
+console.log(apple);
+
+// array.slice() arr.slice([start], [end]) start인덱스 부터 end인덱스까지의 요소를 복사한 새로운 배열을 반환한다.
+
+let slice = ["t", "e", "s", "t"];
+
+console.log(slice.slice(1, 3)); // "e","s" 인덱스가 3인 요소는 제외
+console.log(slice.slice(-2)); // "s", "t" 인덱스가 -2인 요소부터 제일 끝 요소까지 복사
