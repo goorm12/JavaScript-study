671b0e7ac37071c8a0a53ec108f3b3adf964df4e
binwoonki <vinwoonki98@gmail.com>
2024-07-11 22:22:14 +0900

    every() 사용 해보기

--- js/array.js
+++ js/array.js
@@ -21,3 +21,18 @@ console.log(fruits2);
 console.log(fruits3);
 console.log(fruits4);
 // 중복되는 Cherry가 있다. 하지만 덮어 쓰지 않고 각각의 아이템이 나열된 Cherry가 있다.
+
+// .every() : 배열의 모든 요소가 콜백 테스트를 통해 통과하는지 확힌합니다.
+// 만약 테스트가 하나라도 실패하면, 이후 테스트는 진행하지 않고 'false'를 반환합니다.
+const numbers = [198, 120, 19, 7, 188];
+const isValid = numbers.every((item) => item < 200);
+console.log(isValid);
+
+const user = [
+  { name: "Neo", age: 12, email: "neo@github.dev" },
+  { name: "Dias", age: 27, email: "dias@github.dev" },
+  { name: "Ake", age: 29 },
+];
+
+console.log(user.every((user) => user.email));
+console.log(user.every((user) => user.age));
