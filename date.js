0ae84eaf9c9efdf519ab37c8f7a6cef12dc32f2e
binwoonki <vinwoonki98@gmail.com>
2024-07-11 21:28:51 +0900

    getDayko() 함수 만들기

--- date.js
+++ date.js
@@ -8,10 +8,29 @@ console.log(date);
 
 // 타임스탬프에서 각 정보를 얻을 수 있습니다.
 console.log(date.getFullYear());
-console.log(date.getMonth());
+console.log(date.getMonth() + 1);
 // getMonth() 는 숫자를 0부터 시작하기 때문에 1월은 0부터 시작
-console.log(date.getDate());
-console.log(date.getDay());
+console.log(date.getDate()); // 숫자를 1부터 센다
+console.log(getDayKo(date.getDay())); // 0부터 센다. 0부터 6까지 일요일부터 시작
 console.log(date.getHours());
 console.log(date.getMinutes());
 console.log(date.getSeconds());
+
+function getDayKo(day) {
+  switch (day) {
+    case 0:
+      return "일요일";
+    case 1:
+      return "월요일";
+    case 2:
+      return "화요일";
+    case 3:
+      return "수요일";
+    case 4:
+      return "목요일";
+    case 5:
+      return "금요일";
+    case 6:
+      return "토요일";
+  }
+}
